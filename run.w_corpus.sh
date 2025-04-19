b3c06703d2f757d465d1e60f8b85f71cdc4d6acc
Mamoun <mamoun.imghi@epfl.ch>
2025-04-19 03:51:17 +0200

    Copying coverage reports to appropriate folder

--- run.w_corpus.sh
+++ run.w_corpus.sh
@@ -12,4 +12,9 @@ python3 infra/helper.py run_fuzzer libjpeg-turbo compress_fuzzer --corpus-dir bu
 
 # Generate the coverage report
 python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
-python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_with_initial_seeds --fuzz-target compress_fuzzer
+python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_with_initial_seeds --fuzz-target compress_fuzzer &
+PID=$!
+sleep 180
+kill -SIGINT "$PID"
+
+mv build/out/libjpeg-turbo/report/* ./coverage_report/w_seeds
