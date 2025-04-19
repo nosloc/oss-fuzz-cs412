0cb03ffef48a434c3d8089941ad5c41560a63b2b
Mamoun <mamoun.imghi@epfl.ch>
2025-04-19 20:41:23 +0200

    Upload reports

--- run.w_o_corpus.sh
+++ run.w_o_corpus.sh
@@ -17,7 +17,7 @@ python3 infra/helper.py run_fuzzer libjpeg-turbo compress_fuzzer --corpus-dir bu
 python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
 python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_without_seeds --fuzz-target compress_fuzzer &
 PID=$!
-sleep 180
-kill -SIGINT "$PID"
+sleep 1800
+kill -SIGTERM "$PID"
 
 mv build/out/libjpeg-turbo/report/* ./coverage_report/w_o_seeds
